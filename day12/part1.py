9617efce3fa70cf3dbf1db9670238f448890fc9c
Emma Holle <emholle13@yahoo.com>
2023-12-14 20:51:54 +0000

    day 12 part 1 initial

--- day12/part1.py
+++ day12/part1.py
@@ -2,11 +2,17 @@ from __future__ import annotations
 import networkx as nx
 import itertools
 import re
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from shapely import Polygon, Point, MultiPoint
 from dataclasses import dataclass
 from pathlib import Path
 from typing import List, Optional, Dict, Tuple, NewType
 from pprint import pprint
 from functools import cmp_to_key
+from math import lcm
+from icecream import ic
 
 
 @dataclass

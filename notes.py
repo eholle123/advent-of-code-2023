84d1656ed5c94b8e4722702f43ff9e8f9d21c2a7
Emma Holle <emholle13@yahoo.com>
2023-12-15 17:23:49 +0000

    notes update

--- notes.py
+++ notes.py
@@ -13,3 +13,26 @@ from pprint import pprint
 from functools import cmp_to_key
 from math import lcm
 from icecream import ic
+
+
+def read_input(input_file: Path) -> List[str]:
+    with input_file.open("r") as f:
+        lines = f.readlines()
+    return [l.strip() for l in lines]
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    # lines = read_input(Path("inputs/test_input.txt"))
+    lines = read_input(Path("inputs/input.txt"))
+    ic(lines)
+   
+    """
+    Correct answer for Part 1: 
+    """
+
+    print("--- %s seconds ---" % round((time.time() - start_time), 2))
+
+    
+
